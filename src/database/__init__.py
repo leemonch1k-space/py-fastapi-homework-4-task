52149e8cf2bf0b3155fc300d7bf006139cea5c3a
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-02 16:31:19 +0200

    resolved import error

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -8,7 +8,8 @@ from database.models.accounts import (
     ActivationTokenModel,
     PasswordResetTokenModel,
     RefreshTokenModel,
-    UserProfileModel
+    UserProfileModel,
+    GenderEnum
 )
 from database.models.movies import (
     MovieModel,
